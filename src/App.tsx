be2f2b6f43e228b898576b5a0a7ef1bae5b17ecc
German Cocca <coccagerman@gmail.com>
2022-03-22 23:31:56 -0300

    Creating remote repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.scss'
+import logo from './assets/logo.png'
 
-function App() {
+const App:React.FC = () => {
+
+  const [searchInput, setSearchInput] = useState<string>('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt='Logo' />
       </header>
+
+      <section className='intro'>
+        <p>This is my personal repository of sources of knowledge and great places to learn about code.</p>
+        <p>Here I've consolidated most of the resources I've found useful for my learning process, and I plan to keep adding new things as I find them along my path to become the best developer I can be.</p>
+        
+        <p>Feel free to browse through the different cattegories or search for specific topics.</p>
+        <p>Recommendations are always welcome. You can email them to <a href='mailto:coccagerman@gmail.com'>coccagerman@gmail.com</a></p>
+      </section>
+
+      <section className='searchBar'>
+          <input type='text' onChange={e => setSearchInput(e.target.value)} placeholder='Search...' />
+      </section>
+
+      <section className='sectionsBtns'>
+          <button>Design</button>
+          <button>CSS</button>
+          <button>Javascript</button>
+          <button>React</button>
+          <button>Node</button>
+          <button>General programming</button>
+          <button>Algorithms and data structures</button>
+          <button>Tools</button>
+          <button>Others</button>
+          <button>Jobs</button>
+      </section>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/content/design.jsx
@@ -0,0 +1,418 @@
+const designResources = [
+    {
+        name: 'Css-awards',
+        paidOrFree: 'Free',
+        type: 'Design',
+        url: 'https://css-awards.com/',
+        description: 'Awarded website designs to get inspiration from.'
+    },
+    {
+        name: 'Awwwards',
+        paidOrFree: 'Free',
+        type: 'Design',
+        url: 'https://www.awwwards.com/',
+        description: 'Awarded website designs to get inspiration from.'
+    },
+    {
+        name: 'Web.dev',
+        paidOrFree: 'Free',
+        type: 'Design',
+        url: 'https://web.dev/',
+        description: 'Design / development guide.'
+    },
+    {
+        name: 'Material',
+        paidOrFree: 'Free',
+        type: 'Design',
+        url: 'https://material.io/',
+        description: 'Design / development guide.'
+    },
+    {
+        name: 'Cssbattle',
+        paidOrFree: 'Free',
+        type: 'Code challenges / Interview prep',
+        url: 'https://cssbattle.dev/',
+        description: 'CSS code challenges.'
+    },
+    {
+        name: 'Devchallenges',
+        paidOrFree: 'Free',
+        type: 'Code challenges / Interview prep',
+        url: 'https://devchallenges.io/',
+        description: 'Full stack project challenges.'
+    },
+    {
+        name: 'Codewars',
+        paidOrFree: 'Free',
+        type: 'Code challenges / Interview prep',
+        url: 'https://www.codewars.com/',
+        description: 'Coding challenges.'
+    },
+    {
+        name: 'Hackerrank',
+        paidOrFree: 'Free',
+        type: 'Code challenges / Interview prep',
+        url: 'https://www.hackerrank.com/',
+        description: 'Coding challenges.'
+    },
+    {
+        name: 'Leetcode',
+        paidOrFree: 'Free',
+        type: 'Code challenges / Interview prep',
+        url: 'https://www.leetcode.com/',
+        description: 'Coding challenges.'
+    },
+    {
+        name: 'Codepen',
+        paidOrFree: 'Free',
+        type: 'Code playground',
+        url: 'https://codepen.io/',
+        description: 'Code playground.'
+    },
+    {
+        name: 'Jsfiddle',
+        paidOrFree: 'Free',
+        type: 'Code playground',
+        url: 'https://jsfiddle.net/',
+        description: 'Code playground.'
+    },
+    {
+        name: 'Cssgradient',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://cssgradient.io/',
+        description: 'CSS gradient generator.'
+    },
+    {
+        name: 'Tinypng',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://tinypng.com/',
+        description: 'Image compressor.'
+    },
+    {
+        name: 'Pixlr',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://pixlr.com/',
+        description: 'Image editor.'
+    },
+    {
+        name: 'Colorhunt',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://colorhunt.co/',
+        description: 'Colors explorer.'
+    },
+    {
+        name: 'Coolors',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://coolors.co/',
+        description: 'Colors explorer.'
+    },
+    {
+        name: 'Materialpalette',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://www.materialpalette.com/colors/',
+        description: 'Colors explorer.'
+    },
+    {
+        name: 'Fontpair',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://fontpair.co/',
+        description: 'Fonts explorer.'
+    },
+    {
+        name: 'Google fonts',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://fonts.google.com/',
+        description: 'Fonts explorer.'
+    },
+    {
+        name: 'W3 validator',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://validator.w3.org/',
+        description: 'Web markup validator.'
+    },
+    {
+        name: 'Brandmark',
+        paidOrFree: 'Free / Paid',
+        type: 'Tools',
+        url: 'https://brandmark.io/',
+        description: 'Logo generator.'
+    },
+    {
+        name: 'Namelix',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://namelix.com/',
+        description: 'Name generator.'
+    },
+    {
+        name: 'Getwaves',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://getwaves.io/',
+        description: 'SVG waves generator.'
+    },
+    {
+        name: 'Svgwave',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://www.svgwave.in/',
+        description: 'SVG waves generator.'
+    },
+    {
+        name: 'Blobmaker',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://www.blobmaker.app/',
+        description: 'SVG blobs generator.'
+    },
+    {
+        name: 'Shapedivider',
+        paidOrFree: 'Free',
+        type: 'Tools',
+        url: 'https://www.shapedivider.app/',
+        description: 'SVG dividers generator.'
+    },
+    {
+        name: 'Loading.io',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://loading.io/',
+        description: 'Loading animations library.'
+    },
+    {
+        name: 'Iconify',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://iconify.design/',
+        description: 'Free icons.'
+    },
+    {
+        name: 'Flaticon',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://www.flaticon.es/',
+        description: 'Free icons.'
+    },
+    {
+        name: 'Freeillustrations',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://freeillustrations.xyz/',
+        description: 'Free illustrations.'
+    },
+    {
+        name: 'Graphicburger',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://graphicburger.com/',
+        description: 'Free illustrations.'
+    },
+    {
+        name: 'Undraw',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://undraw.co/',
+        description: 'Free illustrations.'
+    },
+    {
+        name: 'Pixabay',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://pixabay.com/',
+        description: 'Free images.'
+    },
+    {
+        name: 'Unsplash',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://unsplash.com/',
+        description: 'Free images.'
+    },
+    {
+        name: 'Wunderstock',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://wunderstock.com/',
+        description: 'Free images.'
+    },
+    {
+        name: 'Freepik',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://www.freepik.es/',
+        description: 'Free images.'
+    },
+    {
+        name: 'Pexels',
+        paidOrFree: 'Free',
+        type: 'Resources',
+        url: 'https://www.pexels.com/',
+        description: 'Free images.'
+    },
+    {
+        name: 'Css-tricks',
+        paidOrFree: 'Free',
+        type: 'Content publication',
+        url: 'https://css-tricks.com/',
+        description: 'Articles on CSS, JS and front-end development in general.'
+    },
+    {
+        name: 'Hacker News',
+        paidOrFree: 'Free',
+        type: 'Content publication',
+        url: 'https://news.ycombinator.com/',
+        description: 'Tech news.'
+    },
+    {
+        name: 'Product hunt',
+        paidOrFree: 'Free',
+        type: 'Content publication',
+        url: 'https://www.producthunt.com/',
+        description: 'Hot newly released tech products.'
+    },
+    {
+        name: 'Mozilla docs',
+        paidOrFree: 'Free',
+        type: 'Documentation',
+        url: 'https://developer.mozilla.org/',
+        description: 'Great documentation about pretty much anything on the internet.'
+    },
+    {
+        name: 'W3schools',
+        paidOrFree: 'Free',
+        type: 'Documentation',
+        url: 'https://www.w3schools.com/',
+        description: 'Documentation about many different topics.'
+    },
+    {
+        name: 'Eloquentjavascript',
+        paidOrFree: 'Free',
+        type: 'Books and publications',
+        url: 'https://eloquentjavascript.net',
+        description: 'JS book.'
+    },
+    {
+        name: 'Javascripttutorial',
+        paidOrFree: 'Free',
+        type: 'Books and publications',
+        url: 'https://www.javascripttutorial.net/',
+        description: 'Detailed tutorials about the features of javascript as a programming language.'
+    },
+    {
+        name: 'Freecodecamp',
+        paidOrFree: 'Free',
+        type: 'Learn to code.',
+        url: 'https://www.freecodecamp.org/',
+        description: 'Free resources to learn to code.'
+    },
+    {
+        name: 'Sololearn',
+        paidOrFree: 'Free',
+        type: 'Learn to code.',
+        url: 'https://www.sololearn.com/',
+        description: 'Free resources to learn to code.'
+    },
+    {
+        name: 'The Odin project',
+        paidOrFree: 'Free',
+        type: 'Learn to code.',
+        url: 'https://www.theodinproject.com/',
+        description: 'Free resources to learn to code.'
+    },
+    {
+        name: 'Brainstation',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://brainstation.io/',
+        description: 'Resources to learn to code.'
+    },
+    {
+        name: 'Mimo',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://getmimo.com/',
+        description: 'Resources to learn to code.'
+    },
+    {
+        name: 'Team tree house',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://teamtreehouse.com/',
+        description: 'Resources to learn to code.'
+    },
+    {
+        name: 'Platzi',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://platzi.com/',
+        description: 'Resources to learn to code (for spanish speakers).'
+    },
+    {
+        name: 'Codecademy',
+        paidOrFree: 'Free / Paid',
+        type: 'Learn to code.',
+        url: 'https://www.codecademy.com/',
+        description: 'Resources to learn to code.'
+    },
+    {
+        name: 'Datacamp',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://www.datacamp.com/',
+        description: 'Resources to learn to code (focused on data science).'
+    },
+    {
+        name: 'Crehana',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://www.crehana.com/',
+        description: 'Online courses about many different topics (for spanish speakers).'
+    },
+    {
+        name: 'Udemy',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://www.udemy.com/',
+        description: 'Online courses about many different topics.'
+    },
+    {
+        name: 'Udacity',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://www.udacity.com/',
+        description: 'Online courses about many different topics.'
+    },
+    {
+        name: 'Udacity',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://www.udacity.com/',
+        description: 'Online courses about many different topics.'
+    },
+    {
+        name: 'Egghead',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://egghead.io/',
+        description: 'Resources to learn to code.'
+    },
+    {
+        name: 'Kentcdodds',
+        paidOrFree: 'Paid',
+        type: 'Learn to code.',
+        url: 'https://kentcdodds.com/',
+        description: 'Coding courses and blog.'
+    },
+
+]
+
+export default designResources
